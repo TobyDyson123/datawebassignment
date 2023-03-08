e523bd54f6de17f8b87cee85a8d72d7c2dc685ec
TobyDyson123 <tobyrdyson@ntlworld.com>
2023-03-08 16:24:36 +0000

    created homepage

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,18 @@ function Home() {
         <div className="home">
             <div className="home-container">
                 <div className="search-container">
-                    Search here
+                    <div className="search-box">
+                        search here
+                    </div>
                 </div>
                 <div className="title-container">
-                    Motor Market
+                    <h1>Find your newest motor with us!</h1>
+                </div>
+                <div className="about-text">
+                    <h2>With plenty of vehicles to choose from, you will always...</h2>
+                </div>
+                <div className="about-text">
+                    <h2>drive away happy!</h2>
                 </div>
             </div>
         </div>
